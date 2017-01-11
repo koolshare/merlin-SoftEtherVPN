5c9d39e202043934c2bd1443d665c00d38f9e2b2
lixin <sadoneli@gmail.com>
2017-01-12 00:27:36 +0800

    first commit for softether vpn

---
+++ config.json.js
@@ -0,0 +1,9 @@
+{
+    "build_date": "2016-11-08_08:10:02", 
+    "description": "SoftEther_VPN_Server_version_4.2.2_build_9634", 
+    "home_url": "Module_softether.asp", 
+    "md5": "a9ed91dab3710f008ca10e7cb2e417ce", 
+    "module": "softether", 
+    "title": "SoftEther_VPN_Server", 
+    "version": "1.3"
+}
