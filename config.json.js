87fc88f890b709210aa65b0ede6cbf5c0ec6f5a7
sadoneli <sadoneli@gmail.com>
2018-04-09 15:40:47 +0800

    update softethervpn

--- config.json.js
+++ config.json.js
@@ -1,9 +1,9 @@
 {
     "build_date": "2016-11-08_08:10:02", 
-    "description": "SoftEther_VPN_Server_version_4.2.2_build_9634", 
+    "description": "SoftEther VPN Server version 4.2.2 build 9634", 
     "home_url": "Module_softether.asp", 
-    "md5": "3bd53fb1e7cbdb377963d434cb27d9bd", 
+    "md5": "5527d23f9ced2fd8507475b335a924bf", 
     "module": "softether", 
     "title": "SoftEther_VPN_Server", 
-    "version": "1.4"
+    "version": "1.5"
 }
