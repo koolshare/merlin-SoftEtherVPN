9e4e61729b759b5cf7fd1e2367ff82528d3c6edf
sadoneli <sadoneli@gmail.com>
2019-04-24 15:36:58 +0800

    update SoftEtherVPN server to 4.29 build 9680

--- backup.sh
+++ backup.sh
@@ -2,16 +2,13 @@
 
 # you can do something here
 # this shell scripts will run at the end of build.py scripts
-
-
-
 mkdir -p history
 if [ ! -f ./history/version ];then
 	touch ./history/version
 fi
 
 version_old=`cat history/version | awk '{print $1}' | sort -rn |sed -n 1p`
-version_new=`cat config.json.js |grep -w "version"|cut -d"\"" -f 4`
+version_new=`cat config.json.js |grep -w "version"|cut -d"\"" -f 4|grep -v Soft`
 md5_old=`cat history/version | sort -nk1 | awk '{print $1}' |sed -n 1p`
 md5_new=` md5sum softether.tar.gz | awk '{print $1}'`
 
--- config.json.js
+++ config.json.js
@@ -1,9 +1,9 @@
 {
     "build_date": "2016-11-08_08:10:02", 
-    "description": "SoftEther VPN Server version 4.2.2 build 9634", 
+    "description": "SoftEtherVPN Server version 4.29 build 9680", 
     "home_url": "Module_softether.asp", 
-    "md5": "5527d23f9ced2fd8507475b335a924bf", 
+    "md5": "9d48115a65319d69ed3bbdf913d68c05", 
     "module": "softether", 
-    "title": "SoftEther_VPN_Server", 
-    "version": "1.5"
+    "title": "SoftEtherVPN_Server", 
+    "version": "1.6"
 }
